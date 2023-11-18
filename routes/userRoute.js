a30ff5a20f534c80020eaadfb2fdbf4f3a3af07d
Vivek Kumar <vivekr4400@gmail.com>
2023-11-18 11:10:38 +0530

    resolved issue getting on render

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -1,6 +1,6 @@
 const express=require('express');
 
-const {Register,Login,getallUser}=require('../controllers/usercontroller')
+const {Register,Login,getallUser}=require('../controllers/userControllerr')
 
 
 
